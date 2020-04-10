02b226906675df5dac1fb0011e219ea369b69646
Rafael Silva <rafonsosilva@gmail.com>
2020-04-09 23:08:08 -0400

    creates guardian class, address class and overrides __str__ method of all classes

--- Tutoring_Management_System/main.py
+++ Tutoring_Management_System/main.py
@@ -1,12 +1,21 @@
-from class_student import Student
+from objects import Student, Guardian, Address
 import datetime
 
 def main():
-    st = Student("Rafael", "Afonso", "Silva", datetime.datetime(1985, 9, 1),
-        "5 Michael Power Place", "rafonso@gmail.com", "active",
-        "John", "Mary", "School Name GBC", 5, ["Math, English"], True,
+    guardAddress = Address("Carlos Olympio Tostes, 479", "", "Araraquara", "14807-210")
+    stAddress = Address("5 Michael Power Place", "309", "Etobicoke", "M9A0A3")
+
+    guard1 = Guardian("John", "", "Afonso", datetime.date(1956, 2, 29),
+        guardAddress, "adao@fs.com", "father", "416-123-1234")
+
+    guard2 = Guardian("Mary", "", "Silva", datetime.date(1960, 6, 23),
+        guardAddress, "mary@fs.com", "mother", "416-123-1234")
+
+    st = Student("Rafael", "Afonso", "Silva", datetime.date(1985, 9, 1),
+        stAddress, "rafonso@gmail.com", "active",
+        guard1, guard2, "School Name GBC", 5, ["Math, English"], True,
         False, "", "Saturday", ["Bloor Collegiate Institute"])
-    st.printStudentInfo()
+    print(st)
 
 
 if __name__ == "__main__":
---
+++ Tutoring_Management_System/objects.py
@@ -0,0 +1,67 @@
+from abc import ABCMeta
+
+class Address:
+    def __init__(self, addressLine1, addressLine2, city, postalCode):
+        self.addressLine1 = addressLine1
+        self.addressLine2 = addressLine2
+        self.city = city
+        self.postalCode = postalCode
+    def __str__(self):
+        out = "{} {} {} {}".format(self.addressLine1, self.addressLine2, self.city, self.postalCode)
+        return out
+
+class Person(metaclass = ABCMeta):
+    currPersonId = 1000000
+    def __init__(self, fName, mName, lName, dateOfBirth, address, email):
+        self.personId = self.currPersonId+1
+        self.firstName = fName
+        self.middleName = mName
+        self.lastName = lName
+        self.dateOfBirth = dateOfBirth
+        self.address = address
+        self.email = email
+
+class Student(Person):
+    def __init__(self, fName, mName, lName, dateOfBirth, address, email, 
+        studentStatus, mainGuardian, secondaryGuardian, schoolCurrEnrolled,
+        gradeLevel, subjectsForHelp, needsOneOnOne, hasDisability, 
+        disabilityDetails, availableDays, preferredSchools):
+            super().__init__(fName, mName, lName, dateOfBirth, address, email)
+            self.studentStatus = studentStatus
+            self.mainGuardian = mainGuardian
+            self.secondaryGuardian = secondaryGuardian
+            self.schoolCurrEnrolled = schoolCurrEnrolled
+            self.disabilityDetails = disabilityDetails
+            self.availableDays = availableDays
+            self.preferredSchools = preferredSchools
+    
+    def __str__(self):
+        output = "{} {} {} {} {} {} {} {} {}".format(
+            self.firstName, self.middleName, self.lastName,
+            self.dateOfBirth.strftime("%x"),
+            self.address,
+            self.studentStatus,
+            self.mainGuardian,
+            self.secondaryGuardian,
+            self.schoolCurrEnrolled
+        )
+        
+        return output
+
+class Guardian(Person):
+    def __init__(self, fName, mName, lName, dateOfBirth, address, email, 
+        guardianType, cellphone, students = ""):
+            super().__init__(fName, mName, lName, dateOfBirth, address, email)
+            self.guardianType = guardianType
+            self.cellphone = cellphone
+            self.students = students
+    def __str__(self):
+        output = "{} | {} | {} | {} | {} | {} | {}".format(
+            self.firstName, self.middleName, self.lastName,
+            self.dateOfBirth,
+            self.address,
+            self.email,
+            self.cellphone
+        )
+        
+        return output
