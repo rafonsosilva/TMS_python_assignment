387d0f5202df496482a6bb8b0346dddab390566e
Rafael Silva <rafonsosilva@gmail.com>
2020-04-14 20:28:27 -0400

    Updates the following classes: Person, Guardian, Student. Creates the following classes: Subject, Weekday, School.

--- Tutoring_Management_System/objects.py
+++ Tutoring_Management_System/objects.py
@@ -1,68 +1,80 @@
 from abc import ABCMeta
 
-class Address:
-    def __init__(self, addressLine1, addressLine2, city, postalCode):
-        self.addressLine1 = addressLine1
-        self.addressLine2 = addressLine2
-        self.city = city
-        self.postalCode = postalCode
-    def __str__(self):
-        out = "{} | {} | {} | {}".format(self.addressLine1, self.addressLine2, self.city, self.postalCode)
-        return out
-
 class Person(metaclass = ABCMeta):
     currPersonId = 1000000
-    def __init__(self, fName, mName, lName, dateOfBirth, address, email):
+    def __init__(self, fName, lName, email):
         self.personId = self.currPersonId+1
         self.firstName = fName
-        self.middleName = mName
         self.lastName = lName
-        self.dateOfBirth = dateOfBirth
-        self.address = address
         self.email = email
 
 class Student(Person):
-    def __init__(self, fName, mName, lName, dateOfBirth, address, email, 
-        studentStatus, mainGuardian, secondaryGuardian, schoolCurrEnrolled,
-        gradeLevel, subjectsForHelp, needsOneOnOne, availableDays, preferredSchools):
-            super().__init__(fName, mName, lName, dateOfBirth, address, email)
-            self.studentStatus = studentStatus
-            self.mainGuardian = mainGuardian
-            self.secondaryGuardian = secondaryGuardian
-            self.schoolCurrEnrolled = schoolCurrEnrolled
+    def __init__(self, fName, lName, email, gradeLevel):
+            super().__init__(fName, lName, email)
             self.gradeLevel = gradeLevel
-            self.subjectForHelp = subjectsForHelp
-            self.needsOneOnOne = needsOneOnOne
-            self.availableDays = availableDays
-            self.preferredSchools = preferredSchools
-    
+
     def __str__(self):
-        output = "{} | {} | {} | {} | {} | {} | {} | {} | {}".format(
-            self.firstName, self.middleName, self.lastName,
-            self.dateOfBirth.strftime("%x"),
-            self.address,
-            self.studentStatus,
-            self.mainGuardian,
-            self.secondaryGuardian,
-            self.schoolCurrEnrolled
+        output = "{} | {} | {} | {}".format(
+            self.firstName, 
+            self.lastName, 
+            self.email, 
+            self.gradeLevel
         )
-        
         return output
 
 class Guardian(Person):
-    def __init__(self, fName, mName, lName, dateOfBirth, address, email, 
-        guardianType, cellphone, students = ""):
-            super().__init__(fName, mName, lName, dateOfBirth, address, email)
+    def __init__(self, fName, lName, email, guardianType, isMainContact):
+            super().__init__(fName, lName, email)
             self.guardianType = guardianType
-            self.cellphone = cellphone
-            self.students = students
+            self.isMainContact = isMainContact
+
     def __str__(self):
-        output = "{} | {} | {} | {} | {} | {} | {}".format(
-            self.firstName, self.middleName, self.lastName,
-            self.dateOfBirth,
-            self.address,
+        output = "{} | {} | {} | {} | {}".format(
+            self.firstName, self.lastName,
             self.email,
-            self.cellphone
+            self.guardianType,
+            self.isMainContact
         )
-        
+        return output
+
+class Tutors(Person):
+    def __init__(self, fName, lName, email):
+        super().__init__(fName, lName, email)
+
+    def __str__(self):
+        output = "{} | {} | {}".format(
+            self.firstName, 
+            self.lastName,
+            self.email
+        )
+        return output
+
+class Subjects():
+    def __init__(self, subjectName):
+        self.subjectName = subjectName
+
+    def __str__(self):
+        output = "{}".format(self.subjectName)
+        return output
+
+class Weekdays():
+    def __init__(self, weekdayName):
+        self.weekdayName = weekdayName
+    
+    def __str__(self):
+        output = "{}".format(self.weekdayName)
+        return output
+
+class Schools():
+    def __init__(self, schoolName, schoolPhone, schoolEmail):
+        self.schoolName = schoolName
+        self.schoolPhone = schoolPhone
+        self.schoolEmail = schoolEmail
+
+    def __str__(self):
+        output = "{} | {} | {}".format(
+            self.schoolName,
+            self.schoolPhone,
+            self.schoolEmail
+        )
         return output
