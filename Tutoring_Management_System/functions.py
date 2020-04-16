4ffd3594cbd5df844d8ad4ce6318b4fe0ec03fc6
Rafael Silva <rafonsosilva@gmail.com>
2020-04-16 02:49:41 -0400

    Program almost finished.

--- Tutoring_Management_System/functions.py
+++ Tutoring_Management_System/functions.py
@@ -1,26 +1,106 @@
+from objects import *
+
 def main_menu():
     print("*****TUTORING MANAGEMENT SYSTEM*****")
-    print("1. Work with Students")
-    print("2. Work with Parents")
-    print("3. Work with Tutors")
-    print("3. Work with Schools")
-    print("4. List Students-Tutors Matches")
+    print("1. View Students List")
+    print("2. Add New Student")
+    print("3. View Guardians List")
+    print("4. Add New Guardian")
+    print("5. View Tutors List")
+    print("6. Add New Tutors")
+    print("7. View Student-Tutor Matches")
     while True:
-        option = int(input("Option: "))
+        option = int(input("Type the menu number form the list above or 0 to exit: "))
         if option == 1:
-            students_menu()
+            listStudents()
+        elif option == 2:
+            addStudent()
+        elif option == 3:
+            searchGuardians()
+        elif option == 4:
+            addGuardian()
+        elif option == 5:
+            listTutors()
+        elif option == 6:
+            addTutor()
+        elif option == 7:
+            listMatches()
+        elif option == 0:
+            print("Exiting the program. See you next time!")
             break
         else:
             print("Not a valid option. Please try again.")
 
-def students_menu():
-    print("*****STUDENTS MENU*****")
-    print("1. Add Student")
-    print("2. Edit Student Info")
-    while True:
-        option = int(input("Option: "))
-        if option == 1:
-            students_menu()
-            break
+def addStudent():
+    print("**********Warning**********")
+    print("The student's guardian must be registered before add the new Student!")
+    option = input("Please enter below the Student's information or enter \"cancel\" to return: ")
+    if option.lower() == "cancel":
+        main_menu()
+    else:
+        firstName = input("First Name: ")
+        lastName = (input("Last Name: "))
+        email = (input("Email: "))
+        guardianToSearch = input("Enter the Guardian First or Last Name to search: (Press enter to list all Guardians)")
+        searchGuardians(guardianToSearch)
+        guardianId = input("Enter the Guardian ID from the list above: ")
+        gradeLevel = input("Grade level: ")
+        listSubjects()
+        subjectId = input("Enter the Subject ID from the list above: ")
+        st = Student(firstName,lastName,email,guardianId,gradeLevel,subjectId)
+        print(st)
+        confirmInput = input("The student above will be added. Press Enter to confirm or type \"cancel\" to return.")
+        if confirmInput.lower() == "cancel":
+            print("New Student not added.")
+            main_menu()
         else:
-            print("Not a valid option. Please try again.")
+            st.commitStudent()
+            print("New Student successfully added.")
+            
+def addGuardian():
+    option = input("Please enter below the Guardian's information or enter \"cancel\" to return: ")
+    if option.lower() == "cancel":
+        main_menu()
+    else:
+        firstName = input("First Name: ")
+        lastName = (input("Last Name: "))
+        email = (input("Email: "))
+        guardianType = (input("Guardian type: "))
+        isMainContact = (input("Is the guardian the main contact with the student (yes/no): "))
+        gd = Guardian(firstName,lastName,email,guardianType,isMainContact)
+        print(gd)
+        confirmInput = input("The Guarduan above will be added. Press Enter to confirm or type \"cancel\" to return.")
+        if confirmInput.lower() == "cancel":
+            print("New Guardian not added.")
+            main_menu()
+        else:
+            gd.commitGuardian()
+            print("New Guardian successfully added.")
+            
+def addTutor():
+    option = input("Please enter below the Tutor's information or enter \"cancel\" to return: ")
+    if option.lower() == "cancel":
+        main_menu()
+    else:
+        firstName = input("First Name: ")
+        lastName = (input("Last Name: "))
+        email = (input("Email: "))
+        minGradeLevel = (input("Min grade level to teach: "))
+        maxGradeLevel = (input("Max grade level to teach: "))
+        listSubjects()
+        subjectId = input("Enter the Subject ID from the list above: ")
+        tt = Tutor(firstName,lastName,email,minGradeLevel,maxGradeLevel,subjectId)
+        print(tt)
+        confirmInput = input("The Tutor above will be added. Press Enter to confirm or type \"cancel\" to return.")
+        if confirmInput.lower() == "cancel":
+            print("New Tutor not added.")
+            main_menu()
+        else:
+            tt.commitTutor()
+            print("New Tutor successfully added.")            
+        
+        
+    
+    
+        
+                
--- Tutoring_Management_System/objects.py
+++ Tutoring_Management_System/objects.py
@@ -1,16 +1,120 @@
 from abc import ABCMeta
 import sqlite3
 from contextlib import closing
+import TableIt #source: https://github.com/CodeForeverAndEver/TableIt
+
 
 DB_FILE = "C:/Users/rafon/Documents/GBC/COMP2152_Python/Database/tutoringManagementSystem.sqlite3"
 conn = sqlite3.connect(DB_FILE)
+
+def searchGuardians(name=""):
+    with closing(conn.cursor()) as c:
+        query = '''SELECT guardian_id, first_name, last_name, email, guardian_type, is_main_contact 
+                    FROM Guardians
+                    WHERE upper(first_name) LIKE upper("%{}%")
+                    OR upper(last_name) LIKE upper("%{}%");'''
+        c.execute(query.format(name, name))
+        guardians = c.fetchall()
+        if guardians[0] is not None:
+            guardiansList = [["Guardian ID","First name","Last Name","Email","Type","Is Main Contact?"]]
+            for guardian in guardians:
+                guardiansList.append([guardian[0],guardian[1],guardian[2],guardian[3],guardian[4],guardian[5]])
+            print("**************GUARDIANS LIST**************")
+            TableIt.printTable(guardiansList, useFieldNames=True)
+        else:
+            print("No results found.")
+
+def listSubjects():
+    with closing(conn.cursor()) as c:
+        query = '''SELECT subject_id, subject_name
+                    FROM Subjects;'''
+        c.execute(query)
+        subjects = c.fetchall()
+        if subjects[0] is not None:
+            subjectsList = [["Subject ID","Subject name"]]
+            for subject in subjects:
+                subjectsList.append([subject[0],subject[1]])
+            print("\n****************************SUBJECTS LIST****************************")
+            TableIt.printTable(subjectsList, useFieldNames=True)
+        else:
+            print("No subjects were found.")
+            
+def listStudents():
+    with closing(conn.cursor()) as c:
+        query = '''SELECT DISTINCT Students.student_id,
+                        Students.first_name, 
+                        Students.last_name, 
+                        Students.email, 
+                        Students.grade_level, 
+                        Guardians.First_Name || ' ' || Guardians.Last_Name as "Guardian Name",
+                        Subjects.subject_name
+                    FROM Students
+                    LEFT JOIN Family ON Family.student_id = Students.student_id
+                    LEFT JOIN Guardians ON Family.guardian_id = Guardians.guardian_id
+                    LEFT JOIN Subjects ON Subjects.subject_id = Students.subject_id;'''
+        c.execute(query)
+        students = c.fetchall()
+        if students is not None:
+            studentsList = [["Student ID","First name", "Last name", "Email", "Grade Level", "Guardian Name", "Subject for Help"]]
+            for student in students:
+                studentsList.append([student[0],student[1],student[2],student[3],student[4],student[5],student[6]])
+            print("\n****************************SUBJECTS LIST****************************")
+            TableIt.printTable(studentsList, useFieldNames=True)
+        else:
+            print("No subjects were found.")
+            
+def listTutors():
+    with closing(conn.cursor()) as c:
+        query = '''SELECT tutor_id,
+                        first_name, 
+                        last_name, 
+                        email, 
+                        min_grade_level, max_grade_level, 
+                        Subjects.subject_name
+                    FROM Tutors
+                    LEFT JOIN Subjects ON Tutors.subject_id = Subjects.subject_id;'''
+        c.execute(query)
+        tutors = c.fetchall()
+        if tutors is not None:
+            tutorsList = [["Tutor ID","First name", "Last name", "Email", "Grade Level Range", "Subject"]]
+            for tutor in tutors:
+                tutorsList.append([tutor[0],tutor[1],tutor[2],tutor[3],(str(tutor[4])+"-"+str(tutor[5])),tutor[6]])
+            print("\n****************************TUTORS LIST****************************")
+            TableIt.printTable(tutorsList, useFieldNames=True)
+        else:
+            print("No subjects were found.")            
+
+def listMatches():
+    with closing(conn.cursor()) as c:
+        query = '''SELECT Students.student_id, 
+            		Students.first_name||' '||Students.last_name AS "Student Name",
+                    Students.grade_level,
+                    Subjects.subject_name,
+                    Tutors.first_name||' '||Tutors.last_name AS "Tutor Name",
+                    Tutors.min_grade_level||'-'||Tutors.max_grade_level AS "Grade Level Range",
+                    Subjects.subject_name
+                  FROM Students
+                  LEFT JOIN Subjects on Subjects.subject_id = Students.subject_id
+                  LEFT JOIN Tutors on Subjects.subject_id = Tutors.subject_id
+                  WHERE Students.grade_level >= Tutors.min_grade_level 
+                      AND Students.grade_level <= Tutors.max_grade_level
+                      AND Students.subject_id LIKE Tutors.subject_id;'''
+        c.execute(query)
+        matches = c.fetchall()
+        if matches is not None:
+            matchesList = [["Student ID","Student name", "Grade Level", "Subject","Tutor Name", "Grade Level Range", "Subject"]]
+            for match in matches:
+                matchesList.append([match[0],match[1],match[2],match[3],match[4],match[5],match[6]])
+            print("\n****************************MATCHES LIST****************************")
+            TableIt.printTable(matchesList, useFieldNames=True)
+        else:
+            print("No matches were found.")
 
 #Get last used ID from Database and return next available ID
 def getNewId(personId, tableName):
         with closing(conn.cursor()) as c:
             query = '''SELECT MAX({}) FROM {};'''
             c.execute(query.format(personId,tableName))
-            #c.execute(query,(personId,tableName,))
             result = c.fetchone()
             if result[0] is None:
                newId = 1
@@ -27,12 +131,11 @@ class Person(metaclass = ABCMeta):
 
 #Guardian Class
 class Guardian(Person):
-    def __init__(self, fName, lName, email, guardianType, isMainContact, students_list):
+    def __init__(self, fName, lName, email, guardianType, isMainContact):
             super().__init__(fName, lName, email)
             self.guardianId = getNewId("guardian_id","Guardians")
             self.guardianType = guardianType
             self.isMainContact = isMainContact
-            self.students_list = students_list
             
     def commitGuardian(self):
         with closing(conn.cursor()) as c:
@@ -48,47 +151,57 @@ class Guardian(Person):
          conn.commit()
 
     def __str__(self):
-        output = "{} | {} | {} | {} | {} | {} | {}".format(
+        output = "Guardian ID: {}\nFirst Name: {}\nLast Name: {}\nEmail: {}\nType {}\nMain Contact: {}".format(
             self.guardianId,
             self.firstName, 
             self.lastName,
             self.email,
             self.guardianType,
-            self.isMainContact,
-            self.students_list[0]
+            self.isMainContact
         )
         return output
 
 #Student Class
 class Student(Person):
         
-    def __init__(self, fName, lName, email, guardians, 
-        gradeLevel, subjectsForHelp):
+    def __init__(self, fName, lName, email, guardian, 
+        gradeLevel, subjectForHelp):
             super().__init__(fName, lName, email)
             self.studentId = getNewId("student_id","Students")
             self.gradeLevel = gradeLevel
-            self.guardians = guardians
-            self.subjectsForHelp = subjectsForHelp
+            self.guardian = guardian
+            self.subjectForHelp = subjectForHelp
     
     def commitStudent(self):
         with closing(conn.cursor()) as c:
-         query = '''INSERT INTO "main"."Students"(
-                         "first_name","last_name","email","grade_level") 
-                    VALUES (?,?,?,?);'''
-         c.execute(query, 
+            query1 = '''INSERT INTO "main"."Students"(
+                         "first_name","last_name","email","grade_level","subject_id") 
+                    VALUES (?,?,?,?,?);'''
+            c.execute(query1, 
                    (self.firstName,
                    self.lastName,
                    self.email,
-                   self.gradeLevel,))
-         conn.commit()
+                   self.gradeLevel,
+                   self.subjectForHelp))
+            conn.commit()
+            query2 = '''INSERT INTO "main"."Family"(
+                         "student_id","guardian_id") 
+                    VALUES (?,?);'''
+            c.execute(query2, 
+                   (self.studentId,
+                   self.guardian,))
+            conn.commit()
+            
 
     def __str__(self):
-        output = "{} | {} | {} | {} | {}".format(
+        output = "ID: {}\nFirst Name: {}\nLast Name: {}\nEmail: {}\nGrade Level: {}\nGuardian ID: {}\nSubject for help: {}".format(
             self.studentId,
             self.firstName, 
             self.lastName, 
             self.email, 
-            self.gradeLevel
+            self.gradeLevel,
+            self.guardian,
+            self.subjectForHelp
         )
         return output
 
@@ -104,18 +217,19 @@ class Tutor(Person):
     def commitTutor(self):
         with closing(conn.cursor()) as c:
          query = '''INSERT INTO "main"."Tutors"(
-                         "first_name","last_name","email","min_grade_level", "max_grade_level") 
-                    VALUES (?,?,?,?,?);'''
+                         "first_name","last_name","email","min_grade_level", "max_grade_level","subject_id") 
+                    VALUES (?,?,?,?,?,?);'''
          c.execute(query, 
                    (self.firstName,
                    self.lastName,
                    self.email,
                    self.minGradeLevel,
-                   self.maxGradeLevel,))
+                   self.maxGradeLevel,
+                   self.subjectToHelp))
          conn.commit()
     
     def __str__(self):
-        output = "{} | {} | {} | {} | {} | {}".format(
+        output = "Tutor Id: {}\nFirst Name: {}\nLast Name: {}\nEmail: {}\nMin Grade Level: {}\nMax Grade Level: {}\nSubject: {}".format(
             self.tutorId,
             self.firstName, 
             self.lastName,
